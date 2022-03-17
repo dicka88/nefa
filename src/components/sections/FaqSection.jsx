87a751f78a67f183a041e38620dc5de3c7a4a64f
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-17 21:30:06 +0700

    Refactor: faq item

--- src/components/sections/FaqSection.jsx
+++ src/components/sections/FaqSection.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
 import { BsChevronUp, BsChevronDown } from 'react-icons/bs';
 
 import faq from '../../images/illustrations/faq.png';
 
+const FaqItem = ({ open, title, children }) => {
+  const [isOpen, setIsOpen] = useState(open ? true : false);
+
+  const iconClass = classNames({
+    'transition-all duration-300': true,
+    'rotate-180': isOpen
+  });
+
+  const contentClass = classNames({
+    'text-gray transition-all duration-300 overflow-hidden': true,
+    'h-full': isOpen,
+    'h-0': !isOpen
+  });
+
+  return (
+    <div className='mb-3 border-b border-lightgray pb-3' >
+      <div className='flex justify-between py-3 cursor-pointer hover:text-primary' onClick={() => setIsOpen(!isOpen)}>
+        {title}
+        <BsChevronUp className={iconClass} />
+      </div>
+      <div className={contentClass}>
+        <p className="select-none">
+          {children}
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default function FaqSection() {
   return (
     <div className="container mx-auto py-32">
-      <div className="grid grid-cols-2">
+      <div className="grid md:grid-cols-2">
         <img src={faq} alt="" />
         <div className="flex justify-center">
           <div className="">
@@ -14,35 +44,20 @@ export default function FaqSection() {
             <h2 className="font-bold text-4xl mb-6 leading-normal">
               Frequently asked questions
             </h2>
-            <ul className="my-6">
-              <li className='mb-3 border-b border-lightgray pb-3'>
-                <div className='flex justify-between py-3'>
-                  Why should I choose NEFA?
-                  <BsChevronUp />
-                </div>
-                <p className="text-gray">
-                  We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.
-                </p>
-              </li>
-              <li className='mb-3 border-b border-lightgray pb-3'>
-                <div className='flex justify-between py-3'>
-                  How secure is NEFA?
-                  <BsChevronDown />
-                </div>
-              </li>
-              <li className='mb-3 border-b border-lightgray pb-3'>
-                <div className='flex justify-between py-3'>
-                  Do I have to buy a whole Bitcoin?
-                  <BsChevronDown />
-                </div>
-              </li>
-              <li className='mb-3 border-b border-lightgray pb-3'>
-                <div className='flex justify-between py-3'>
-                  How do I actually buy Bitcoin?
-                  <BsChevronDown />
-                </div>
-              </li>
-            </ul>
+            <div className="my-6">
+              <FaqItem open={true} title="Why should I choose NEFA?">
+                We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.
+              </FaqItem>
+              <FaqItem open={false} title="How secure is NEFA?">
+                We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.
+              </FaqItem>
+              <FaqItem open={false} title="Do I have to buy a whole Bitcoin?">
+                We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.
+              </FaqItem>
+              <FaqItem open={false} title="How do I actually buy Bitcoin?">
+                We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.
+              </FaqItem>
+            </div>
           </div>
         </div>
       </div>
