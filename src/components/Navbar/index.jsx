3c4c3990a86d31b1b774b80aea335f0ab088b464
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-18 10:08:37 +0700

    Feat(dropdown): add nav menu dropdown

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { BsChevronDown, BsList } from 'react-icons/bs';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { BsChevronDown, BsList, BsX } from 'react-icons/bs';
 
 import logo from '../../images/logo.svg';
 import PrimaryButton from '../buttons/PrimaryButton';
 import SecondaryButton from '../buttons/SecondaryButton';
 
 export default function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const dropdownClass = classNames({
+    'text-base left-0 top-full right-0 absolute transition-all duration-400': true,
+    'invisible opacity-0': !dropdownOpen,
+    'visible opacity-100': dropdownOpen,
+  });
+
   return (
-    <nav className='bg-primary bg-opacity-5'>
+    <nav className={`relative transition-all duration-400 ${dropdownOpen ? 'bg-white' : 'bg-primary'} bg-opacity-5`}>
       <div className="sticky px-4 container mx-auto top-0 py-8 flex justify-between items-center">
         <div className="flex items-center">
           <img src={logo} className="mr-6" alt="Neva" />
@@ -23,8 +32,8 @@ export default function Navbar() {
             </a>
           </div>
         </div>
-        <div className="hidden md:flex">
-          <SecondaryButton className="mr-4">
+        <div className="hidden gap-4 md:flex">
+          <SecondaryButton>
             Sign In
           </SecondaryButton>
           <PrimaryButton>
@@ -32,9 +41,52 @@ export default function Navbar() {
           </PrimaryButton>
         </div>
         <div className="md:hidden text-2xl">
-          <button>
-            <BsList />
+          <button className="z-50 p-4 block transition-all" onClick={() => setDropdownOpen(!dropdownOpen)}>
+            {dropdownOpen ?
+              <BsX />
+              :
+              <BsList />
+            }
           </button>
+
+          {/* Menu dropdown */}
+          <div className={dropdownClass}>
+            <div className="h-screen left-0 bg-black bg-opacity-30">
+              <div className="z-20 shadow-xl bg-white p-6">
+                <div className="gap-4 flex mb-6">
+                  <SecondaryButton className="w-full">
+                    Sign In
+                  </SecondaryButton>
+                  <PrimaryButton className="w-full">
+                    Sign Up
+                  </PrimaryButton>
+                </div>
+                <div className="mb-4">
+                  <div className="w-full">
+                    <a href="/" className='hover:text-primary py-2 block'>Cryptocurrency</a>
+                  </div>
+                  <div className="w-full">
+                    <a href="/" className='hover:text-primary py-2 block'>Exchanges</a>
+                  </div>
+                  <div className="w-full">
+                    <a href="/" className='hover:text-primary py-2 block'>Watchlist</a>
+                  </div>
+                  <div className="w-full">
+                    <a href="/" className='hover:text-primary py-2 block'>NFT</a>
+                  </div>
+                  <div className="w-full">
+                    <a href="/" className='hover:text-primary py-2 block'>Portofolios</a>
+                  </div>
+                  <div className="w-full">
+                    <a href="/" className='hover:text-primary py-2 flex justify-between'>
+                      Product
+                      <BsChevronDown className="inline ml-2" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
