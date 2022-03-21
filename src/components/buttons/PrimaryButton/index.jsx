9d049aa605b55c0fa6204f1caaa71f689e96369c
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-21 17:40:50 +0700

    Move button

--- src/components/buttons/PrimaryButton/index.jsx
+++ src/components/buttons/PrimaryButton/index.jsx
@@ -2,10 +2,10 @@ import classNames from 'classnames';
 import React from 'react';
 
 export default function PrimaryButton({ children, className, onClick, ...props }) {
-  const cx = classNames([
+  const cx = classNames(
     "text-white rounded-full py-3 px-6 md:py-4 md:px-10 bg-gradient-to-br from-primary to-secondary",
     className
-  ]);
+  );
   return (
     <button onClick={onClick} className={cx} {...props}>
       {children}
