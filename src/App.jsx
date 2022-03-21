e5e3455209393fa18bc7aed94e5627a2d8ca8878
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-21 15:22:47 +0700

    Feat: react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import Index from './pages/index';
+
+import Index from '@/pages/index';
+import CryptoCurrencyPage from '@/pages/CryptoCurrencyPage';
+import Error404 from '@/pages/Error404';
 
 function App() {
   return (
-    <Index />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="cryptocurrency" element={<CryptoCurrencyPage />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
