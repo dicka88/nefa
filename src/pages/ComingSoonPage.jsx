7291262a6cc4c452d477facaad70ca38027d764f
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-21 15:23:06 +0700

    Add page

---
+++ src/pages/ComingSoonPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ComingSoonPage() {
+  return (
+    <div>ComingSoonPage</div>
+  );
+}
---
+++ src/pages/Error500.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Error500() {
+  return (
+    <div>Error500</div>
+  );
+}
