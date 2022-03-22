dab1ad185da2369ef7f14d8840c285fe52e77633
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-23 01:41:57 +0700

    Set page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Lottie from 'react-lottie-player';
+
+import Layout from '@/components/Layout';
+import error404Animation from '@/lottie/404-website-error-animation.json';
 
 export default function Error404() {
   return (
-    <div>Error404</div>
+    <Layout>
+      <div className="flex justify-center h-screen items-center">
+        <div className="text-center">
+          <Lottie animationData={error404Animation} loop play />
+          <span className="text-lg text-primary">We cannot find your page</span>
+        </div>
+      </div>
+    </Layout>
   );
 }
--- src/pages/Error500.jsx
+++ src/pages/Error500.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Lottie from 'react-lottie-player';
+
+import Layout from '@/components/Layout';
+import error500Animation from '@/lottie/500-website-error-animation.json';
 
 export default function Error500() {
   return (
-    <div>Error500</div>
+    <Layout>
+      <div className="flex justify-center h-screen items-center">
+        <div className="text-center">
+          <Lottie animationData={error500Animation} loop play />
+          <span className="text-lg text-primary">Internal Server Error :(</span>
+        </div>
+      </div>
+    </Layout>
   );
 }
