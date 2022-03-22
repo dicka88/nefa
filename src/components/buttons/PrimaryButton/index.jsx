f0a3a976575ba61fb02e3510b58259950d179da1
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-22 22:38:20 +0700

    Add hover state animation

--- src/components/buttons/PrimaryButton/index.jsx
+++ src/components/buttons/PrimaryButton/index.jsx
@@ -2,13 +2,14 @@ import classNames from 'classnames';
 import React from 'react';
 
 export default function PrimaryButton({ children, className, onClick, ...props }) {
-  const cx = classNames(
-    "text-white rounded-full py-3 px-6 md:py-4 md:px-10 bg-gradient-to-br from-primary to-secondary",
-    className
-  );
   return (
-    <button onClick={onClick} className={cx} {...props}>
+    <button onClick={onClick} className={classNames(
+      "text-white rounded-full py-3 px-6 md:py-4 md:px-10 bg-gradient-to-br hover:bg-gradient-to-r transition-all duration-300 from-primary to-secondary hover:bg-blend-darken",
+      className
+    )}
+      {...props}
+    >
       {children}
-    </button>
+    </button >
   );
 }
