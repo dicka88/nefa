1cf91a8e25a67ffe2631dc35c3342f9c29cd3f36
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-22 22:41:16 +0700

    Add new component

---
+++ src/components/Select/index.jsx
@@ -0,0 +1,69 @@
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { BsChevronDown } from 'react-icons/bs';
+import OutsideClickHandler from 'react-outside-click-handler';
+
+export default function Select({ onChange, value = "BTC" }) {
+  const data = [{
+    name: 'BTC',
+    code: 'BTC',
+    icon: '/cryptocurrencies/btc.svg'
+  }, {
+    name: 'USD',
+    code: 'USD',
+    icon: '/cryptocurrencies/usd.svg'
+  }, {
+    name: 'ETH',
+    code: 'ETH',
+    icon: '/cryptocurrencies/etherium.png'
+  }];
+
+  // find initial coin
+  const initialCoin = data.find(({ code }) => code === value);
+
+  const [valueSelected, setValueSelected] = useState(initialCoin);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onChangeHandler = (item) => {
+    setValueSelected(item);
+    setIsOpen(false);
+    if (typeof onChange === 'function') {
+      onChange(item);
+    }
+  };
+
+  return (
+    <div className="relative">
+      <div className="border border-primary cursor-pointer rounded-2xl py-3 md:py-4 px-4 md:px-6 flex items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className='w-[26px] pr-2'>
+          <img src={valueSelected.icon} alt={valueSelected.name} className="flex items-center" />
+        </div>
+        <span className='inline-block mr-2'>{valueSelected.name}</span>
+        <BsChevronDown />
+      </div>
+
+      <OutsideClickHandler onOutsideClick={() => isOpen && setIsOpen(false)}>
+        <div className={classNames('absolute top-full right-0 left-0 bg-white z-[2] mt-4 rounded-xl shadow-xl border border-primary overflow-hidden', {
+          'invisible opacity-0': !isOpen,
+          'visible opacity-100': isOpen
+        })}>
+          <div>
+            {data.map(coin =>
+              <div className={classNames('py-2 flex items-center cursor-pointer hover:text-white select-none px-4 hover:bg-primary', {
+                'bg-primary text-white': coin.name === valueSelected.name
+              })} onClick={() => onChangeHandler(coin)}>
+                <div className='h-[26px] aspect-square pr-2'>
+                  <img src={coin.icon} alt={coin.name} className="flex items-center" />
+                </div>
+                <span className='inline-block mr-2'>{coin.name}</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </OutsideClickHandler>
+
+    </div >
+  );
+}
---
+++ src/components/buttons/DropdownButton/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { BsChevronDown } from 'react-icons/bs';
+import OutsideClickHandler from 'react-outside-click-handler';
+
+import SecondaryButton from '@/components/buttons/SecondaryButton';
+import classNames from 'classnames';
+
+export default function DropdownButton({ className, name, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <SecondaryButton className={className} onClick={() => setIsOpen(!isOpen)}>
+        {name}
+        <BsChevronDown className="inline ml-2" />
+      </SecondaryButton>
+      <OutsideClickHandler onOutsideClick={() => isOpen && setIsOpen(false)}>
+        <div className={
+          classNames('absolute left-0 right-0 top-full transition-all duration-200 z-[1] overflow-hidden mt-2 border border-primary rounded-3xl bg-white shadow-xl', {
+            'opacity-100 visible': isOpen,
+            'opacity-0 invisible': !isOpen
+          })}>
+          {children}
+        </div>
+      </OutsideClickHandler>
+    </div>
+  );
+}
