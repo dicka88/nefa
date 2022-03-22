4de3a18e4f72307e3e60ba824115be417c9fb74e
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-23 00:34:51 +0700

    Fix: wrap inside <form>

--- src/components/sections/BuyAndTradeSection.jsx
+++ src/components/sections/BuyAndTradeSection.jsx
@@ -6,6 +6,11 @@ import PrimaryButton from '@/components/buttons/PrimaryButton';
 import Select from '../Select';
 
 export default function BuyAndTradeSection() {
+
+  const formHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="container mx-auto py-32">
       <div className="grid md:grid-cols-2 gap-10">
@@ -14,25 +19,27 @@ export default function BuyAndTradeSection() {
             <h2 className="font-bold text-4xl mb-6 leading-normal">Buy & trade on the <br /> original crypto exchange.</h2>
             <p className="text-gray mb-6">Buy now and get 40% extra bonus Minimum pre-sale amount <br /> 25 Crypto Coin. We accept BTC crypto-currency</p>
 
-            <div className="flex justify-between gap-4 md:gap-6 mb-6">
-              <div className="border border-primary rounded-2xl py-3 md:py-4 px-4 md:px-6 flex items-center">
-                <div className="border-r border-primary pr-4 md:pr-6">
-                  <small className="text-primary">Amount</small>
+            <form onSubmit={formHandler}>
+              <div className="flex justify-between gap-4 md:gap-6 mb-6">
+                <div className="border border-primary rounded-2xl py-3 md:py-4 px-4 md:px-6 flex items-center">
+                  <div className="border-r border-primary pr-4 md:pr-6">
+                    <small className="text-primary">Amount</small>
+                  </div>
+                  <input type="text" value="5,000" className='text-right outline-none w-full' />
                 </div>
-                <input type="text" value="5,000" className='text-right outline-none w-full' />
+                <Select value="USD" />
               </div>
-              <Select value="USD" />
-            </div>
 
-            <div className="flex justify-between gap-4 md:gap-6 mb-6">
-              <div className="border border-primary rounded-2xl py-3 md:py-4 px-4 md:px-6 flex items-center w-full">
-                <div className="border-r border-primary pr-4 md:pr-6">
-                  <small className="text-primary">Amount</small>
+              <div className="flex justify-between gap-4 md:gap-6 mb-6">
+                <div className="border border-primary rounded-2xl py-3 md:py-4 px-4 md:px-6 flex items-center w-full">
+                  <div className="border-r border-primary pr-4 md:pr-6">
+                    <small className="text-primary">Amount</small>
+                  </div>
+                  <input type="text" value="0.10901" className='text-right outline-none w-full' />
                 </div>
-                <input type="text" value="0.10901" className='text-right outline-none w-full' />
+                <Select value="BTC" />
               </div>
-              <Select value="BTC" />
-            </div>
+            </form>
 
             <PrimaryButton className="w-full">
               Buy Now
